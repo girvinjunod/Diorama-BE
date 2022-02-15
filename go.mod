f7e2f859b4c857c1649b3f3944e4174af4240203
girvinjunod <girvinjunod@gmail.com>
2022-02-15 13:06:18 +0700

    feat: add initial database, API for get user by ID

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/gofiber/fiber/v2 v2.26.0
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/lib/pq v1.10.4
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.33.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- index.go
+++ index.go
@@ -1,13 +1,97 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "diorama"
+	password = "diorama"
+	dbname   = "diorama"
+)
 
 func main() {
+	log.Println("Starting Server")
 	app := fiber.New()
 
+	app.Static("/", "./public")
+
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	// open database
+	db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// check db
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Succesfully connected to database")
+
+	// insertDynStmt := `insert into users (username, email, password, profile_picture) values($1,$2,$3,$4)`
+	// _, err = db.Exec(insertDynStmt, "scrooge", "scrooge@gmail.com", "scrooge", "profile-picture/rich.jpg")
+	// CheckError(err)
+
 	app.Get("/", func(c *fiber.Ctx) error {
+		log.Println("Hello")
 		return c.SendString("Hello, World 👋!")
 	})
+
+	app.Get("/getUserByID/:id", func(c *fiber.Ctx) error {
+		type userResponse struct {
+			Id              int    `json:"id"`
+			Username        string `json:"username"`
+			Email           string `json:"email"`
+			Profile_picture string `json:"profile_picture"`
+		}
+		id := c.Params("id")
+		query := `SELECT * FROM users where id=$1`
+		rows, err := db.Query(query, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer rows.Close()
+		var response string
+		for rows.Next() {
+			var id int
+			var username string
+			var email string
+			var password string
+			var profile_picture string
+			if err := rows.Scan(&id, &username, &email, &password, &profile_picture); err != nil {
+				log.Fatal(err)
+			}
+
+			res := &userResponse{
+				Id:              id,
+				Username:        username,
+				Email:           email,
+				Profile_picture: profile_picture,
+			}
+
+			reply, _ := json.Marshal(res)
+			response = string(reply)
+
+			log.Println(response)
+		}
+		return c.SendString(response)
+	},
+	)
 
 	app.Listen(":3000")
 }
